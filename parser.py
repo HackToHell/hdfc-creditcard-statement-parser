b3ec1090fcb2da2325bc7ba34ef2551e679a3af9
Gowtham Sundara <gowtham.sundara@rapyuta-robotics.com>
2021-05-22 16:29:35 +0530

    Update to support the new statement.

--- parser.py
+++ parser.py
@@ -1,30 +1,33 @@
 #!/usr/bin/env python3
+import argparse
+import csv
+import locale
+import os
 from datetime import date, datetime
 from typing import NamedTuple, List
-import csv
+from dateutil.parser import parse
+
 import tabula
-import sys
-import os
-import locale
-import argparse
 
 locale.setlocale(locale.LC_ALL, 'en_US.UTF-8')
 
 # HDFC Dinner 10X rewards
 DINERS_SMARTBUY_PARTNERS = [
-  'SMARTBUYBANGALORE',
-  'FLIPKART PAYMENTSBANGALORE', 
-  'WWW GYFTR COMGURGAON',
-  'SMARTBUY VOUCHERSNEW DELHI',
-  'IRCTC SMART BUYBANGALORE',
-  'AMAZON SELLER SERVICES MUMBAI'
+    'SMARTBUYBANGALORE',
+    'FLIPKART PAYMENTSBANGALORE',
+    'WWW GYFTR COMGURGAON',
+    'SMARTBUY VOUCHERSNEW DELHI',
+    'IRCTC SMART BUYBANGALORE',
+    'AMAZON SELLER SERVICES MUMBAI'
 ]
+
 
 class Transaction(NamedTuple):
     received: date
     details: str
     amount: float
     transaction_type: str
+
 
 class TransactionWithRewards(NamedTuple):
     received: date
@@ -34,7 +37,6 @@ class TransactionWithRewards(NamedTuple):
     rewards: float
 
 
-_DATE_FORMAT = "%d/%m/%Y"
 
 # Convert Amount to Number
 def try_sanitize_amount(amnts):
@@ -44,17 +46,19 @@ def try_sanitize_amount(amnts):
     except ValueError:
         return None
 
+
 # Parse Date
 def try_parse_date(ds: str):
     try:
-        return datetime.strptime(ds, _DATE_FORMAT)
+        return parse(ds)
     except:
         return None
 
+
 # parses credit card statement
-def yield_credit_infos(fname: str, show_diners_rewards: bool):
-    
-    res = tabula.read_pdf(fname ,pages='all', stream=True)
+def yield_credit_infos(fname: str, show_diners_rewards: bool, password: str):
+    res = tabula.read_pdf(fname, pages='all', stream=True, password=password)
+
     def try_transaction(line):
         transaction_date = line[0]
         amount = line[-1]
@@ -78,7 +82,7 @@ def yield_credit_infos(fname: str, show_diners_rewards: bool):
         if show_diners_rewards:
             diners_rewards = 0
             if transaction_type == 'credit' and (details.find('IMPS PMT ') != -1 or amount < 100):
-                diners_rewards=0
+                diners_rewards = 0
             elif details in DINERS_SMARTBUY_PARTNERS:
                 diners_rewards = amount * .33
             if transaction_type == 'credit':
@@ -100,16 +104,18 @@ def yield_credit_infos(fname: str, show_diners_rewards: bool):
             )
 
     for page in res:
-      for line in page.values:
-          for t in try_transaction(line):
-              yield t
+        for line in page.values:
+            for t in try_transaction(line):
+                yield t
 
-def get_credit_infos(fname: str, show_diners_rewards: bool) -> List[Transaction]:
-    return list(yield_credit_infos(fname, show_diners_rewards))
+
+def get_credit_infos(fname: str, show_diners_rewards: bool, password: str) -> List[Transaction]:
+    return list(yield_credit_infos(fname, show_diners_rewards, password))
+
 
 def str2bool(v):
     if isinstance(v, bool):
-       return v
+        return v
     if v.lower() in ('yes', 'true', 't', 'y', '1'):
         return True
     elif v.lower() in ('no', 'false', 'f', 'n', '0'):
@@ -117,28 +123,31 @@ def str2bool(v):
     else:
         raise argparse.ArgumentTypeError('Boolean value expected.')
 
+
 def parse_arguments():
     parser = argparse.ArgumentParser()
     parser.add_argument('--statement-path', required=True, type=str,
                         help='path to statements pdf file or directory')
     parser.add_argument('--show-diners-rewards', type=str2bool,
                         help='show diners 10x rewards', default=False)
-
+    parser.add_argument('--password', type=str, help='WTF is the password', required=True)
+    parser.add_argument('--output-file-name', required=True, type=str)
     return parser.parse_args()
 
-def main(pdf_path, show_diners_rewards):
-    infos = []
-    
-    if os.path.isfile(pdf_path):
-        infos = get_credit_infos(pdf_path, show_diners_rewards)
-    else: 
-        files = [f for f in os.listdir(pdf_path)]
-        files = filter(lambda f: f.endswith(('.pdf','.PDF')), files)
-        for f in files:
-            infos.extend(get_credit_infos(os.path.join(pdf_path, f), show_diners_rewards))
 
-    with open('output.csv', 'w') as f:
-        writer = csv.writer(f , lineterminator='\n')
+def parser(pdf_path, show_diners_rewards, password, output_file_name):
+    infos = []
+
+    if os.path.isfile(pdf_path):
+        infos = get_credit_infos(pdf_path, show_diners_rewards, password)
+    else:
+        files = [f for f in os.listdir(pdf_path)]
+        files = filter(lambda f: f.endswith(('.pdf', '.PDF')), files)
+        for f in files:
+            infos.extend(get_credit_infos(os.path.join(pdf_path, f), show_diners_rewards, password))
+
+    with open(output_file_name, 'w') as f:
+        writer = csv.writer(f, lineterminator='\n')
         if show_diners_rewards:
             writer.writerow(('Date', 'Transaction', 'Amount', 'Type', 'Rewards'))
         else:
@@ -146,6 +155,8 @@ def main(pdf_path, show_diners_rewards):
         for tup in infos:
             writer.writerow(tup)
 
+
 if __name__ == '__main__':
     arguments = parse_arguments()
-    main(arguments.statement_path, arguments.show_diners_rewards)
+    parser(arguments.statement_path, arguments.show_diners_rewards, arguments.password,
+           arguments.output_file_name)
