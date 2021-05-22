27f82a29d613847d04b5ca9716193d60bf919ca5
Gowtham Sundara <gowtham.sundara@rapyuta-robotics.com>
2021-05-22 16:38:27 +0530

    make module blah blah

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
 setup(
-   name='hdfc parser',
+   name='hdfcparser',
    version='1.0',
    description='HDFC Credit Card Statement converter to csv',
    author='santosh',
    author_email='santosh.siddarth123@gmail.com',
-   packages=['hdfc-parser'],  #same as name
+   packages=['hdfcparser'],  #same as name
    install_requires=['tabula-py'], #external packages as dependencies
 )
